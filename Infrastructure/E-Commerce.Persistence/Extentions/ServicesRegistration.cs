632aef2a37d1c79b6d2e2083b2bd9ed0b99e0a32
Uygar Eren <uygar_eren2000@hotmail.com>
2023-12-29 17:57:53 +0300

    Added ServicesRegistration extention for IoC container.

---
+++ Infrastructure/E-Commerce.Persistence/Extentions/ServicesRegistration.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Application.Repositories;
+using E_Commerce.Persistence.Context;
+using E_Commerce.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Persistence.Extentions
+{
+	public static class ServicesRegistration
+	{
+		public static void AddPersistenceServices(this IServiceCollection services, string connectionString)
+		{
+			services.AddDbContext<ECommerceDBContext>(options => options.UseNpgsql(connectionString));
+			services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+			services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+
+			services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+			services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+
+			services.AddScoped<IProductReadRepository, ProductReadRepository>();
+			services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+		}
+	}
+}
