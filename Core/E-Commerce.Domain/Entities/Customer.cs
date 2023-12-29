89c614bc08b381d64ff74988be28ef08f7edeb83
Uygar Eren <uygar_eren2000@hotmail.com>
2023-12-29 17:52:36 +0300

    Added Domain Models

---
+++ Core/E-Commerce.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Domain.Entities.Common;
+
+namespace E_Commerce.Domain.Entities
+{
+	public class Customer : BaseEntity
+	{
+		public string Name { get; set; }
+		public ICollection<Order> Orders { get; set; }
+	}
+}
---
+++ Core/E-Commerce.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Domain.Entities.Common;
+
+namespace E_Commerce.Domain.Entities
+{
+	public class Order : BaseEntity
+	{
+		public string Description { get; set; }
+		public ICollection<Product> Products { get; set; }
+		public Address Address { get; set; }
+		public Guid CustomerId { get; set; }
+		public Customer Customer { get; set; }
+
+	}
+}
