ce536026248571a0a8e9e7ea24c829db0a774092
Uygar Eren <uygar_eren2000@hotmail.com>
2024-01-09 23:01:21 +0300

    Added Relation With Product and Product Image Entites
    Added a cross table in data base for product and product image files.

--- Core/E-Commerce.Domain/Entities/Product.cs
+++ Core/E-Commerce.Domain/Entities/Product.cs
@@ -8,5 +8,6 @@ namespace E_Commerce.Domain.Entities
 		public int Stock { get; set; }
 		public decimal Price { get; set; }
 		public ICollection<Order> Orders { get; set; }
+		public ICollection<ProductImageFile> ProductImages { get; set; }
 	}
 }
