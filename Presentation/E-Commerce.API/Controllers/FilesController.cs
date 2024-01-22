47cab73e8643b92d3decc64831bf3bdc89804c05
Uygar Eren <uygar_eren2000@hotmail.com>
2024-01-22 23:17:21 +0300

    Added Files Controller

---
+++ Presentation/E-Commerce.API/Controllers/FilesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FilesController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+
+		public FilesController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+
+		[HttpGet("[action]")]
+		public IActionResult GetBaseStorageUrl()
+		{
+
+			return Ok(new { baseStorageUrl = _configuration["StorageBaseUrl"] });
+		}
+	}
+}
